0f39b551a0f6d4267c3ae9af9290baafcf974501
pikoro1234 <donjuan.jfc@gmail.com>
2023-07-18 00:11:46 +0200

    test id torneo 116

--- v2/public/apiBetsapi.php
+++ v2/public/apiBetsapi.php
@@ -1,1 +1,3 @@
-<?php echo "test confirmada bbbbbb";?>
+<?php echo "test confirmada bbbbbb".$_POST['id'];?>
+
+<?php echo "https://api.b365api.com/v1/event/lineup?token=153716-4djEyj4e6JZVou&event_id=".$_POST['id'];?>
