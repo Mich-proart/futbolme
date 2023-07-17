5bf8c0847887d425b2edb7d9d8744d012eddeb7f
pikoro1234 <donjuan.jfc@gmail.com>
2023-07-18 00:38:11 +0200

    test id torneo ccccc

--- v2/public/apiBetsapi.php
+++ v2/public/apiBetsapi.php
@@ -4,24 +4,24 @@
 
 <?php
 // URL del endpoint
-$url = 'https://api.b365api.com/v1/event/lineup?token=153716-4djEyj4e6JZVou&LNG_ID=3&event_id='.$_POST['id'];
+//$url = 'https://api.b365api.com/v1/event/lineup?token=153716-4djEyj4e6JZVou&LNG_ID=3&event_id='.$_POST['id'];
 
 // Inicializar la solicitud cURL
-$ch = curl_init();
+//$ch = curl_init();
 
 // Establecer la URL
-curl_setopt($ch, CURLOPT_URL, $url);
+//curl_setopt($ch, CURLOPT_URL, $url);
 
 // Establecer opciones adicionales según sea necesario, por ejemplo:
 // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Si deseas recibir la respuesta en una variable en lugar de imprimir directamente
 
 // Ejecutar la solicitud
-$response = curl_exec($ch);
+//$response = curl_exec($ch);
 
 // Verificar si hubo algún error en la solicitud
-if (curl_errno($ch)) {
-    echo 'Error en la solicitud: ' . curl_error($ch);
-} else {
+// if (curl_errno($ch)) {
+//     echo 'Error en la solicitud: ' . curl_error($ch);
+// } else {
     // Procesar la respuesta
     //echo json_encode($response);
 
@@ -44,9 +44,41 @@ if (curl_errno($ch)) {
         //     ];         
         // }
 
-        echo json_encode($response);
-}
+//         echo json_encode($response);
+// }
 
 // Cerrar la conexión cURL
-curl_close($ch);
+//curl_close($ch);
+
+$data = $_POST['id'];
+
+$objResponse = [
+    'local' => '',
+    'visitante' => ''
+];
+
+$response = Http::get('https://api.b365api.com/v1/event/lineup?token=153716-4djEyj4e6JZVou&LNG_ID=3&event_id='.$data);
+
+if($response->json()['results']){
+
+    $datosLocal = $response->json()['results']['home']['startinglineup'];
+
+    $datosVisitante = $response->json()['results']['away']['startinglineup'];
+
+    $objResponse = [
+        'local' => $datosLocal,
+        'visitante' => $datosVisitante
+    ];         
+}
+
+return \json_encode($objResponse);
+
+
+
+
+
+
+
+
+
 ?>
