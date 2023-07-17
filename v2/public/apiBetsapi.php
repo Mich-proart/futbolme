8babb47783e6d837e1e42c36ad40feb774acb63a
pikoro1234 <donjuan.jfc@gmail.com>
2023-07-18 00:13:49 +0200

    test id torneo 116

--- v2/public/apiBetsapi.php
+++ v2/public/apiBetsapi.php
@@ -1,3 +1,31 @@
 <?php echo "test confirmada bbbbbb".$_POST['id'];?>
 
 <?php echo "https://api.b365api.com/v1/event/lineup?token=153716-4djEyj4e6JZVou&event_id=".$_POST['id'];?>
+
+<?php
+// URL del endpoint
+$url = 'https://api.b365api.com/v1/event/lineup?token=153716-4djEyj4e6JZVou&event_id='.$_POST['id'];
+
+// Inicializar la solicitud cURL
+$ch = curl_init();
+
+// Establecer la URL
+curl_setopt($ch, CURLOPT_URL, $url);
+
+// Establecer opciones adicionales según sea necesario, por ejemplo:
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Si deseas recibir la respuesta en una variable en lugar de imprimir directamente
+
+// Ejecutar la solicitud
+$response = curl_exec($ch);
+
+// Verificar si hubo algún error en la solicitud
+if (curl_errno($ch)) {
+    echo 'Error en la solicitud: ' . curl_error($ch);
+} else {
+    // Procesar la respuesta
+    echo $response;
+}
+
+// Cerrar la conexión cURL
+curl_close($ch);
+?>
