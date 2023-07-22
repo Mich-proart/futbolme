9c603e8c9ec772dea949f1b935bcdfa797193b85
pikoro1234 <donjuan.jfc@gmail.com>
2023-07-23 00:47:20 +0200

    session update ver 4

--- v2/public/panelBack/funciones/login.php
+++ v2/public/panelBack/funciones/login.php
@@ -1,25 +1,19 @@
 <?php 
-require_once '../includes/config.php';
-//if(isset($_POST['trigger'])){
-    //var_dump($_POST['trigger']);
-    //var_dump($_POST['user']);
-    //var_dump($_POST['password']);
-    $response = '';
-
-    $mysqli = conectar();
-    $consulta = "SELECT * FROM users;";    
-    $resultadoSQL = mysqli_query($mysqli, $consulta);
-    $resultado = mysqli_fetch_all($resultadoSQL, MYSQLI_BOTH);
-    foreach ($resultado as $key => $value) {
-        if ((password_verify($_POST['password'], $value['password'])) && $_POST['user'] == $value['email']) {
-            // Iniciamos session con un valor definido
-            session_start();        
-            $_SESSION['user'] = $_POST['user'];       
-            $response = 'true';
-            die;
-        } else {
-            $response = 'false';
-        }        
-    }
-    echo json_encode($response);
-//}
+require_once '../includes/config.php';    
+$response = '';
+$mysqli = conectar();
+$consulta = "SELECT * FROM users;";    
+$resultadoSQL = mysqli_query($mysqli, $consulta);
+$resultado = mysqli_fetch_all($resultadoSQL, MYSQLI_BOTH);
+foreach ($resultado as $key => $value) {
+    if ((password_verify($_POST['password'], $value['password'])) && $_POST['user'] == $value['email']) {
+        // Iniciamos session con un valor definido
+        session_start();        
+        $_SESSION['user'] = $_POST['user'];       
+        $response = 'true';
+        die;
+    } else {
+        $response = 'false';
+    }        
+}
+echo json_encode($response);
--- v2/public/panelBack/index.php
+++ v2/public/panelBack/index.php
@@ -33,6 +33,7 @@ if (isset($_POST['categoria_torneo'])) {
 //echo "Hola, $nombre. Tienes $edad años.";
 
 // Destruimos la sesión (opcional)
+session_destroy();
 ?>
 
 <?php //var_dump($_SESSION['user']);?>
