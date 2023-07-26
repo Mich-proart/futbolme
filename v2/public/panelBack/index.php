95e195de8f52ecb3c261c0d7a1efc0ccaedc0eff
Michell Arenales <68234112+Mich-proart@users.noreply.github.com>
2023-07-26 22:51:29 +0200

    ajustes de login

--- v2/public/panelBack/index.php
+++ v2/public/panelBack/index.php
@@ -5,173 +5,278 @@ require_once 'includes/get.php';
 require_once 'includes/post.php';
 require_once 'includes/head.php';
 
-$externo=0;
-if (isset($_GET['idu'])){ $externo=($_GET['idu']+1000);}
+$externo = 0;
+if (isset($_GET['idu'])) {
+  $externo = ($_GET['idu'] + 1000);
+}
 
 
-$comunidad_id=$_GET['comunidad_id']??0;
-if ($comunidad_id>0){ $bloque='block'; } else { $bloque='none';} 
+$comunidad_id = $_GET['comunidad_id'] ?? 0;
+if ($comunidad_id > 0) {
+  $bloque = 'block';
+} else {
+  $bloque = 'none';
+}
 
-$tipo_torneo=$_GET['tipo_torneo']??1;
-$categoria_torneo=1;
+$tipo_torneo = $_GET['tipo_torneo'] ?? 1;
+$categoria_torneo = 1;
 if (isset($_POST['categoria_torneo'])) {
-  $categoria_torneo=$_POST['categoria_torneo'];
-  $tipo_torneo=3;
-  
+  $categoria_torneo = $_POST['categoria_torneo'];
+  $tipo_torneo = 3;
 }
 ?>
 </head>
+
 <body>
-<?php
-// Iniciamos la sesión
-//session_start();
+  <?php
+  // Iniciamos la sesión
+  //session_start();
 
-// Accedemos a los datos de la sesión
-//$nombre = $_SESSION['nombre'];
+  // Accedemos a los datos de la sesión
+  //$nombre = $_SESSION['nombre'];
 
-// Puedes utilizar los datos como desees
-//echo "Hola, $nombre. Tienes $edad años.";
+  // Puedes utilizar los datos como desees
+  //echo "Hola, $nombre. Tienes $edad años.";
 
-// Destruimos la sesión (opcional)
-?>
+  // Destruimos la sesión (opcional)
+  ?>
 
-<?php //var_dump($_SESSION['user']);?>
-<?php if(!isset($_SESSION['user'])){ ?>
-<div class="content-form" style="display: flex;justify-content: center;align-items: center;"></div>
-<form style="padding-top: 200px;display: flex;justify-content: center;align-items: center;flex-wrap: wrap;width: 500px;margin: auto;">
-  <div class="form-group" style="width: 100%;">
-    <label style="color: white;" for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmailLogin" aria-describedby="emailHelp">    
-  </div>  
-  <div class="form-group" style="width: 100%;">
-    <label style="color: white;" for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPasswordLogin">
-  </div> 
-  <div class="form-group form-password-incorrecto" style="display:none;width: 100%;">    
-    <label style="color: white;" class="form-check-label-text">* Datos incorrectos</label>
-  </div> 
-  <button style="width: 50%;" type="button" class="btn btn-primary btn-login-panel">Submit</button>
-</form>
-<?php require_once 'includes/ajax.php';?>
-<?php die;?>
-<?php }?>
+  <?php //var_dump($_SESSION['user']);
+  ?>
+  <?php if (!isset($_SESSION['user'])) { ?>
 
-<div style="background-color: white; padding: 5px;">
-  <div id="cache" class="pull-right"></div>
-  <table class="table">
-    <tr>
-      <td><div class="content-inicio btn btn-default" data-related="liga" id="ligaClick">Ligas</div></td>
-      <?php if ($externo==0){ ?>
-      <td><div class="content-inicio btn btn-default" data-related="torneos" id="torneoClick">Torneos</div></td>
-      <td><div class="content-inicio btn btn-default" data-related="directos">Directos</div></td>
-      <td><div class="content-inicio btn btn-default" data-related="agenda">Agenda</div></td>
-      <td><div class="content-inicio btn btn-default" data-related="federaciones">Federaciones</div></td>
-      <td><div class="content-inicio btn btn-default" data-related="calLiga">Crear calendarios</div></td>
-      <td class="hide"><div class="content-inicio btn btn-default" data-related="calTorneo">Crear torneos</div></td>
-      <td><div class="content-inicio btn btn-default" data-related="clubs">Clubs</div></td>
-      <td><div class="content-inicio btn btn-default" data-related="fichajes">Fichajes</div></td>
-      <td><div class="content-inicio btn btn-default" data-related="activa">Jornada Activa</div></td>
-      <td><div class="content-inicio btn btn-default" data-related="medios">Medios</div></td>
-      <td><div class="content-inicio btn btn-default" data-related="historico">Histórico</div></td>
-      <td><div class="content-inicio btn btn-default" data-related="whatsapp">Whatsapp</div></td>
-      <?php } ?>
-    </tr>
-    
-    
-    <?php if ($externo==0){ ?>
-    <tr><td colspan="12" align="right" style="padding: 10px; background-color: lavender">
-    <a href="/panelCargador/cargador.php">Cargador</a> - 
-    <a href="/panelCargador/generarMenu.php" target="blank">Generar menu</a> - 
-    <a href="/panelCargador/urlTorneos.php" title="">Url torneos</a> - 
-    <a href="/panelCargador/limpiezaUsuarios.php" target="blank">Limpieza de usuarios</a> - 
-    <a href="/borrarCacheTWIG" target="blank">Borrar caché TWIG</a> - 
-    <a href="/borrarCacheHTML" target="blank">Borrar caché HTML</a> - 
-    <a href="utiles/generarEquiposBuscador.php" title="">Buscador Equipos</a> - 
-    <a href="utiles/generarJugadoresBuscador.php" title="">Buscador Jugadores</a>
+    <style>
+      html,
+      body {
+        height: 100%;
+      }
 
-    (<a href="https://search.finderant.com/force-index-auto.php?hashKey=14d0a18510c816e8cbfc0dfdba2679ce&site=58" target="_blank">Actualizar.</a>)
+      body {
+        display: -ms-flexbox;
+        display: -webkit-box;
+        display: flex;
+        -ms-flex-align: center;
+        -ms-flex-pack: center;
+        -webkit-box-align: center;
+        align-items: center;
+        -webkit-box-pack: center;
+        justify-content: center;
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+      }
 
-     - 
-    <a href="utiles/guardarTorneos.php" title="">Guardar torneos</a> - 
-    <a style="display: none" href="editor.php" title="">Editores</a> -  
-    <a style="display: none" href="editor.php?t=-1" title="Editor para Iván" class="boldfont" style="color:maroon">Editor Iván</a>    
-    </td></tr>
-    <?php } ?>
+      .form-signin {
+        width: 100%;
+        max-width: 330px;
+        padding: 15px;
+        margin: 0 auto;
+      }
 
-    <tr><td colspan="10" valign="top" class="celestebox">
-      <div id="pruebas"></div>
-    <div id="main-content-inicio">
+      .form-signin .checkbox {
+        font-weight: 400;
+      }
+
+      .form-signin .form-control {
+        position: relative;
+        box-sizing: border-box;
+        height: auto;
+        padding: 10px;
+        font-size: 16px;
+      }
+
+      .form-signin .form-control:focus {
+        z-index: 2;
+      }
+
+      .form-signin input[type="email"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+      }
+
+      .form-signin input[type="password"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+      }
+    </style>
+
+
+    <div class="content-form" style="display: flex;justify-content: center;align-items: center;"></div>
+    <form class="form-signin">
+      <img src="https://futbolme.com/static/img/logo.svg" alt="" class="img-fluid">
+      <h1 class="h3 mb-3 font-weight-normal">Iniciar sesión</h1>
+     
+        <label for="exampleInputEmail1" class="sr-only">Correo Electrónico</label>
+        <input type="email" class="form-control" id="exampleInputEmailLogin" aria-describedby="emailHelp" placeholder="Correo Electrónico" required autofocus>
+   
       
-      <div class="container-inicio index-interno" id="liga" style="display: block;">
-        
+        <label class="sr-only" for="exampleInputPassword1">Contraseña</label>
+        <input type="password" class="form-control" id="exampleInputPasswordLogin"  placeholder="Contraseña" required>
+      
+      <div class="form-group form-password-incorrecto" style="display:none;width: 100%;">
+        <label style="color: white;" class="form-check-label-text">* Datos incorrectos</label>
+      </div>
+
+      <button class="btn btn-lg btn-primary btn-block btn-login-panel" type="button">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 199-2023</p>
+
+
+    </form>
+    <?php require_once 'includes/ajax.php'; ?>
+    <?php die; ?>
+
+
+  <?php } ?>
+
+  <div style="background-color: white; padding: 5px;">
+    <div id="cache" class="pull-right"></div>
+    <table class="table">
+      <tr>
+        <td>
+          <div class="content-inicio btn btn-default" data-related="liga" id="ligaClick">Ligas</div>
+        </td>
+        <?php if ($externo == 0) { ?>
+          <td>
+            <div class="content-inicio btn btn-default" data-related="torneos" id="torneoClick">Torneos</div>
+          </td>
+          <td>
+            <div class="content-inicio btn btn-default" data-related="directos">Directos</div>
+          </td>
+          <td>
+            <div class="content-inicio btn btn-default" data-related="agenda">Agenda</div>
+          </td>
+          <td>
+            <div class="content-inicio btn btn-default" data-related="federaciones">Federaciones</div>
+          </td>
+          <td>
+            <div class="content-inicio btn btn-default" data-related="calLiga">Crear calendarios</div>
+          </td>
+          <td class="hide">
+            <div class="content-inicio btn btn-default" data-related="calTorneo">Crear torneos</div>
+          </td>
+          <td>
+            <div class="content-inicio btn btn-default" data-related="clubs">Clubs</div>
+          </td>
+          <td>
+            <div class="content-inicio btn btn-default" data-related="fichajes">Fichajes</div>
+          </td>
+          <td>
+            <div class="content-inicio btn btn-default" data-related="activa">Jornada Activa</div>
+          </td>
+          <td>
+            <div class="content-inicio btn btn-default" data-related="medios">Medios</div>
+          </td>
+          <td>
+            <div class="content-inicio btn btn-default" data-related="historico">Histórico</div>
+          </td>
+          <td>
+            <div class="content-inicio btn btn-default" data-related="whatsapp">Whatsapp</div>
+          </td>
+        <?php } ?>
+      </tr>
+
+
+      <?php if ($externo == 0) { ?>
+        <tr>
+          <td colspan="12" align="right" style="padding: 10px; background-color: lavender">
+            <a href="/panelCargador/cargador.php">Cargador</a> -
+            <a href="/panelCargador/generarMenu.php" target="blank">Generar menu</a> -
+            <a href="/panelCargador/urlTorneos.php" title="">Url torneos</a> -
+            <a href="/panelCargador/limpiezaUsuarios.php" target="blank">Limpieza de usuarios</a> -
+            <a href="/borrarCacheTWIG" target="blank">Borrar caché TWIG</a> -
+            <a href="/borrarCacheHTML" target="blank">Borrar caché HTML</a> -
+            <a href="utiles/generarEquiposBuscador.php" title="">Buscador Equipos</a> -
+            <a href="utiles/generarJugadoresBuscador.php" title="">Buscador Jugadores</a>
+
+            (<a href="https://search.finderant.com/force-index-auto.php?hashKey=14d0a18510c816e8cbfc0dfdba2679ce&site=58" target="_blank">Actualizar.</a>)
+
+            -
+            <a href="utiles/guardarTorneos.php" title="">Guardar torneos</a> -
+            <a style="display: none" href="editor.php" title="">Editores</a> -
+            <a style="display: none" href="editor.php?t=-1" title="Editor para Iván" class="boldfont" style="color:maroon">Editor Iván</a>
+          </td>
+        </tr>
+      <?php } ?>
+
+      <tr>
+        <td colspan="10" valign="top" class="celestebox">
+          <div id="pruebas"></div>
+          <div id="main-content-inicio">
+
+            <div class="container-inicio index-interno" id="liga" style="display: block;">
+
 
 
               <div id="listaTorneoFutbolme1" style="float:left; position: absolute; top:150px; z-index: 1000; right:10px;" class="col-sm-7 col-xs-12"></div>
               <div style="position: absolute; top:60px;left:20px; max-width: 250px; height: 60px; z-index:0">
-                <span onclick="refrescarLigas()" style="cursor:pointer" class="boldfont" style="font-size: 30px">Limpiar</span> - 
-                Ligas<select name="categoria" onchange="cargar_torneos(this.value,1,<?php echo $externo?>)">
+                <span onclick="refrescarLigas()" style="cursor:pointer" class="boldfont" style="font-size: 30px">Limpiar</span> -
+                Ligas<select name="categoria" onchange="cargar_torneos(this.value,1,<?php echo $externo ?>)">
                   <option value="0" selected>Categorias</option>
                   <option value="1">RFEF</option>
                   <option value="4">Autonómica</option>
-                  <option value="5">Juvenil</option>                
-                  <option value="7">Femenino</option> 
+                  <option value="5">Juvenil</option>
+                  <option value="7">Femenino</option>
                   <option value="9">Europa</option>
-                  <option value="17">Fútbol Sala</option>           
-                  </select>
+                  <option value="17">Fútbol Sala</option>
+                </select>
               </div>
               <div id="listaTorneos1" style="position: absolute; top:150px;left:20px; max-width: auto;"></div>
-      </div>
-      <div class="container-inicio" id="torneos" style="display: none;">
-        
-              <div id="listaTorneoFutbolme2" style="float:left; position: absolute; top:150px; z-index: 1000; right:10px" class="col-sm-7 col-xs-12"></div>        
+            </div>
+            <div class="container-inicio" id="torneos" style="display: none;">
+
+              <div id="listaTorneoFutbolme2" style="float:left; position: absolute; top:150px; z-index: 1000; right:10px" class="col-sm-7 col-xs-12"></div>
               <div style="position: absolute; top:60px;left:20px; max-width: 250px; height: 60px; z-index:0">
-                <span onclick="refrescarTorneos()" style="cursor:pointer" class="boldfont" style="font-size: 30px">Limpiar</span> - 
-                Torneos: <select name="categoria" onchange="cargar_torneos(this.value,2,<?php echo $externo?>)">
-                <option value="0" selected>Categorias</option>
-                <option value="1">RFEF</option>
-                <option value="2">FIFA</option>
-                <option value="3">UEFA</option>
-                <option value="4">Autonómica</option>
-                <option value="5">Juvenil</option>                
-                <option value="7">Femenino</option> 
-                <option value="9">Europa</option>
-                <option value="17">Fútbol Sala</option>           
+                <span onclick="refrescarTorneos()" style="cursor:pointer" class="boldfont" style="font-size: 30px">Limpiar</span> -
+                Torneos: <select name="categoria" onchange="cargar_torneos(this.value,2,<?php echo $externo ?>)">
+                  <option value="0" selected>Categorias</option>
+                  <option value="1">RFEF</option>
+                  <option value="2">FIFA</option>
+                  <option value="3">UEFA</option>
+                  <option value="4">Autonómica</option>
+                  <option value="5">Juvenil</option>
+                  <option value="7">Femenino</option>
+                  <option value="9">Europa</option>
+                  <option value="17">Fútbol Sala</option>
                 </select>
               </div>
               <div id="listaTorneos2" style="position: absolute; top:150px;left:20px; max-width: auto; background-color: gainsboro"></div>
-      </div>
-      <div class="container-inicio" id="directos" style="display: none;">
-              <div id="listaTorneoJornadas" style="float:left; position: absolute; top:150px; z-index: 1000; right:10px"  class="col-sm-7 col-xs-12"></div>
+            </div>
+            <div class="container-inicio" id="directos" style="display: none;">
+              <div id="listaTorneoJornadas" style="float:left; position: absolute; top:150px; z-index: 1000; right:10px" class="col-sm-7 col-xs-12"></div>
               <div id="listaTorneos3" style="position: absolute; top:60px;left:20px; max-width: 400px"></div>
-      </div>
-      
-      <div class="container-inicio" id="agenda" style="display: none;"></div>
+            </div>
 
-      <div class="container-inicio" id="federaciones" style="display: <?php echo $bloque?>;">
-          <?php require_once 'federacion/index.php'?>
-      </div>
-      <div class="container-inicio" id="calLiga" style="display: none;"></div>
-      <div class="container-inicio" id="calTorneo" style="display: none;"></div>
-      <div class="container-inicio" id="clubs" style="display: none;"></div>
-      <div class="container-inicio" id="fichajes" style="display: none;"></div>
-      <div class="container-inicio" id="activa" style="display: none;"></div>
-      <div class="container-inicio" id="medios" style="display: none;"></div>
-      <div class="container-inicio" id="historico" style="display: none;"></div>
-      <div class="container-inicio" id="whatsapp" style="display: none;"></div>
-    </div>
-  </td></tr>
-</table>
+            <div class="container-inicio" id="agenda" style="display: none;"></div>
 
-</div>
+            <div class="container-inicio" id="federaciones" style="display: <?php echo $bloque ?>;">
+              <?php require_once 'federacion/index.php' ?>
+            </div>
+            <div class="container-inicio" id="calLiga" style="display: none;"></div>
+            <div class="container-inicio" id="calTorneo" style="display: none;"></div>
+            <div class="container-inicio" id="clubs" style="display: none;"></div>
+            <div class="container-inicio" id="fichajes" style="display: none;"></div>
+            <div class="container-inicio" id="activa" style="display: none;"></div>
+            <div class="container-inicio" id="medios" style="display: none;"></div>
+            <div class="container-inicio" id="historico" style="display: none;"></div>
+            <div class="container-inicio" id="whatsapp" style="display: none;"></div>
+          </div>
+        </td>
+      </tr>
+    </table>
+
+  </div>
 
 </body>
+
 </html>
 
 
 
-<?php require_once 'includes/ajax.php';?>
-<?php require_once 'funciones/clasificacion/jscolor.php';?>
+<?php require_once 'includes/ajax.php'; ?>
+<?php require_once 'funciones/clasificacion/jscolor.php'; ?>
 
 <script>
-cargar_torneos(1,3,0);
+  cargar_torneos(1, 3, 0);
 </script>
